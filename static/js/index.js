7b219b54466593264398d4b6bb16f74d6aea3792
joshua <joshua@andrews-air.lan>
2018-07-21 08:53:39 -0700

    fix style for footer. add new fonts.

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,35 @@
-console.log("Welcome!");
+var $buttons = $("footer").find("button")
+
+$buttons.click(e => {
+  $(".content").removeClass("hide");
+  var $button = $(e.target);
+  $buttons.removeClass("active");
+  $button.addClass("active");
+  var $content = $("footer > .content");
+  var label = $button.text();
+  switch (label) {
+    case "about":
+      $content.html(aboutHtml);
+      break;
+    case "inquiries":
+      $content.html(inquiriesHtml);
+      break;
+    case "pronunciation":
+      $content.html(pronunciationHtml);
+      break;
+    case "privacy":
+      $content.html(privacyHtml);
+      break;
+    default:
+      return;
+  }
+});
+
+var aboutHtml = `<p>We are a software studio focused on:</p>
+<ol>
+  <li>Creating modular and extensible components for developers.</li>
+  <li>Deliver memorable and engaging experiences for consumers.</li>
+</ol>`;
+var inquiriesHtml = ``;
+var pronunciationHtml = ``;
+var privacyHtml = ``;
